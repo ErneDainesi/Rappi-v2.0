e85df7edc444912127409d25a6e1de85ac9d32f9
ErneDainesi <50252041+ErneDainesi@users.noreply.github.com>
2019-11-22 19:27:40 -0300

    Update carga_predefinida_bin.py

--- carga_predefinida_bin.py
+++ carga_predefinida_bin.py
@@ -1,11 +1,9 @@
 #MODULO CARGA PREDEFINIDA
+#Sobreescribe los archivos ya existentes, si el usuario lo desea.
 
-#Todas estas funciones reciben los diccionarios vacios
-#Devuelven los diccionarios con la informacion 'hardcodeada'
-
-from imprimir_menus import limpiar_pantalla
-from imprimir_mensaje import mensaje_info, mensaje_solicitud
-from info_predefinida import crear_archivos
+from imprimir_menus import limpiar_pantalla, mensaje_info, mensaje_solicitud
+from info_predefinida import crear_archivos_binarios
+from carga_archivos import guardar_datos, vaciar_archivo
 import pickle
 
 def carga_predefinida(restaurantes, clientes, rappitenderos):
@@ -13,12 +11,18 @@ def carga_predefinida(restaurantes, clientes, rappitenderos):
 	mensaje_info("Si realiza una carga predefinida los datos anteriores se perderan.")
 	realizar_carga = mensaje_solicitud("Desea continuar? (s/n): ")
 	if realizar_carga == "s":
-		crear_archivos()
-		leer_binario("restaurantes.bin", restaurantes)
-		leer_binario("clientes.bin", clientes)
-		leer_binario("rappitenderos.bin", rappitenderos)
+		vaciar_archivo("restaurantes.csv")
+		vaciar_archivo("clientes.csv")
+		vaciar_archivo("rappitenderos.csv")
+		crear_archivos_binarios()
+		leer_binario("restaurantes_predefinido.bin", restaurantes)
+		leer_binario("clientes_predefinido.bin", clientes)
+		leer_binario("rappitenderos_predefinido.bin", rappitenderos)
 		limpiar_pantalla()
 		mensaje_info('Se ha realizado una carga predefinida.\n')
+		guardar_datos(restaurantes, "restaurantes.csv")
+		guardar_datos(clientes, "clientes.csv")
+		guardar_datos(rappitenderos, "rappitenderos.csv")
 		return restaurantes, clientes, rappitenderos
 	print(mensaje_info("Volviendo al menu anterior..."))
 
@@ -35,12 +39,12 @@ def leer_binario(nombre_archivo, diccionario_a_cargar):
 	return diccionario_a_cargar
 
 def cargar_diccionario(nombre_archivo, dato, diccionario):
-	if nombre_archivo == "restaurantes.bin":
+	if nombre_archivo == "restaurantes_predefinido.bin":
 		for nombre, direccion, telefono, posicion, radio_de_entrega, platos, total_de_ventas in zip(dato[0], dato[1], dato[2], dato[3], dato[4], dato[5], dato[6]):
 			diccionario[nombre] = {'Direccion' : direccion, 'Telefono' : telefono, 'Posicion': posicion, 'Radio de Entrega': radio_de_entrega, 'Platos': platos, 'Total de ventas' : total_de_ventas}
-	elif nombre_archivo == "clientes.bin":
+	elif nombre_archivo == "clientes_predefinido.bin":
 		for nombre, contrasenia, telefono, direccion, posicion, rappicreditos in zip(dato[0], dato[1], dato[2], dato[3], dato[4], dato[5]):
 			diccionario[nombre] = {'Contrasenia' : contrasenia, 'Telefono' : telefono, 'Direccion' : direccion, 'Posicion' : posicion, 'Rappicreditos' : rappicreditos}
-	elif nombre_archivo == "rappitenderos.bin":
+	elif nombre_archivo == "rappitenderos_predefinido.bin":
 		for nombre, propina_acumulada, posicion_actual, pedido_actual, distancia_recorrida in zip(dato[0], dato[1], dato[2], dato[3], dato[4]):
 			diccionario[nombre] = {'Propina acumulada' : propina_acumulada, 'Posicion actual' : posicion_actual, 'Pedido actual' : pedido_actual, 'Distancia recorrida' : distancia_recorrida}
