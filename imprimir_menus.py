46a9260688f073806709d2001fbb76d047fefc8e
ErneDainesi <50252041+ErneDainesi@users.noreply.github.com>
2019-11-24 15:26:43 -0300

    Update imprimir_menus.py

--- imprimir_menus.py
+++ imprimir_menus.py
@@ -43,7 +43,7 @@ def imprimir_submenu(opcion_elegida):
 	elif opcion_elegida == 2:
 		print('**** PEDIDOS **** \n 1) Pedido manual \n 2) Simulacion de pedidos \n 3) Volver \n')
 	elif opcion_elegida == 3:
-		print('**** INFORMES ****\n 1) Clientes con mas Rappicreditos\n 2) Rappitenderos con mas propina\n 3) Restaurantes con mas ventas \n 4) Generar archivo con formato csv de los rappitenderos y su distancia recorrida \n 5) Volver \n')
+		print('**** INFORMES ****\n 1) Clientes con mas Rappicreditos\n 2) Rappitenderos con mas propina\n 3) Restaurantes con mas ventas \n 4) Distancia recorrida por rappitenderos \n 5) Volver \n')
 
 def imprimir_submenu_carga_manual():
 	print('**** CARGA MANUAL ****\n 1) Restaurante \n 2) Cliente \n 3) Plato \n 4) Rappitendero \n 5) Volver \n')
