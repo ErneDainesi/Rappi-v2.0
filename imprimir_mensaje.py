dea4d1018d4e5ff28284c0b8b55336e51e3b856f
lucasbilo <56201008+lucasbilo@users.noreply.github.com>
2019-11-30 15:56:17 -0300

    Update and rename imprimir_mensaje.py to mensajes_y_validaciones.py

--- imprimir_mensaje.py
+++
@@ -1,8 +0,0 @@
-def mensaje_info(mensaje):
-    print("[INFO]", mensaje)
-
-def mensaje_solicitud(mensaje):
-    return input("[SOLICITUD]" + " " + mensaje)
-
-def mensaje_error(mensaje):
-    print("[ERROR]", mensaje)
